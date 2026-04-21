0fe5f638bb3d3138aa9d0f6b76154585c26b2361
Ardıl Acar <arrabax@gmail.com>
2026-04-21 22:53:58 +0300

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,34 @@
+<<< 'EOF' > ims_source/smali/android/telephony/TelephonyBaseUtilsStub.smali
+.class public Landroid/telephony/TelephonyBaseUtilsStub;
+.super Ljava/lang/Object;
+.method public constructor <init>()V
+    .locals 0
+    invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+    return-void
+.end method
+.method public static getInstance()Landroid/telephony/TelephonyBaseUtilsStub;
+    .locals 1
+    new-instance v0, Landroid/telephony/TelephonyBaseUtilsStub;
+    invoke-direct {v0}, Landroid/telephony/TelephonyBaseUtilsStub;-><init>()V
+    return-object v0
+.end method
+.method public static isMiuiRom()Z
+    .locals 1
+    const/4 v0, 0x0
+    return v0
+.end method
+EOF
+apktool b ims_source -o ims-fixed.apk && java -jar uber-apk-signer.jar -a ims-fixed.apk --allowResign && mkdir -p tmp_ims/system/system_ext/priv-app/ims && cp ims-fixed-aligned-debugSigned.>
+#!/system/bin/sh
+MODDIR=${0%/*}
+mount -o bind $MODDIR/system/system_ext/priv-app/ims/ims.apk /system_ext/priv-app/ims/ims.apk
+rm -rf /data/dalvik-cache/*/*ims*
+EOF
+cat << 'EOF' > tmp_ims/sepolicy.rule
+allow priv_app vendor_hal_telephony_service2 service_manager find
+allow priv_app radio_service service_manager find
+allow priv_app hal_telephony_hwservice hwservice_manager find
+EOF
+chmod 755 tmp_ims/post-fs-data.sh && cd tmp_ims && zip -rq9 ../ksu-ims-injection-v9.zip * && cd .. && #rm -rf tmp_ims ims_source ims-fixed.apk ims.apk ims-fixed-aligned-debugSigned.apk 
+echo -e "\n✅ Success! Flash ksu-ims-injection-v9.zip"
+
